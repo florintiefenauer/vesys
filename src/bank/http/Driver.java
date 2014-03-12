1248c6bd47399a35d22d209986d31f6c327bb95e
flo <rueegg.florian@gmail.com>
2014-03-12 15:55:52 +0100

    package band.http

---
+++ src/bank/http/Driver.java
@@ -0,0 +1,36 @@
+package bank.http;
+
+import java.io.IOException;
+
+import bank.Bank;
+import bank.BankDriver;
+
+public class Driver implements BankDriver {
+
+	@Override
+	public void connect(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Bank getBank() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
